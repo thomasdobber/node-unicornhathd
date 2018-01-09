1ae370db2965db1e3223cc9e539e6a91f6e1e3e7
Thomas Dobber <thomasdobber@gmail.com>
2018-01-09 11:14:38 +0100

    moved r g b value checks to setPixel method

--- index.js
+++ index.js
@@ -34,18 +34,6 @@ UnicornHatHD.prototype.getBrightness = function () {
 };
 
 UnicornHatHD.prototype.setAll = function (r, g, b) {
-    if (r !== parseInt(r, 10)) {
-        throw 'r must be an integer';
-    }
-
-    if (g !== parseInt(g, 10)) {
-        throw 'r must be an integer';
-    }
-
-    if (b !== parseInt(b, 10)) {
-        throw 'b must be an integer';
-    }
-
     for (var x=0; x<16; x++) {
         for (var y=0; y<16; y++) {
             this.matrix[x][y].r = r;
@@ -56,6 +44,9 @@ UnicornHatHD.prototype.setAll = function (r, g, b) {
 };
 
 UnicornHatHD.prototype.setPixel = function (x, y, r, g, b) {
+    if (r !== parseInt(r, 10)) { throw 'r must be an integer'; }
+    if (g !== parseInt(g, 10)) { throw 'r must be an integer'; }
+    if (b !== parseInt(b, 10)) { throw 'b must be an integer'; }
     this.matrix[x][y].r = r;
     this.matrix[x][y].g = g;
     this.matrix[x][y].b = b;
@@ -84,23 +75,26 @@ UnicornHatHD.prototype.show = function (flip_horizontal, flip_vertical) {
         this.matrix.reverse();
     }
 
-    var buffer = new Buffer(16 * 16 * 3);
-
-    for (var y=0; y<16; y++) {
-        for (var x=0; x<16; x++) {
-            buffer[y * 16 * 3 + x * 3 + 0] = this.matrix[x][y].r * this.brightness;
-            buffer[y * 16 * 3 + x * 3 + 1] = this.matrix[x][y].g * this.brightness;
-            buffer[y * 16 * 3 + x * 3 + 2] = this.matrix[x][y].b * this.brightness;
-        }
-    }
-
     if(!this.debugMode) {
+        
+        var buffer = new Buffer(16 * 16 * 3);
+
+        for (var y=0; y<16; y++) {
+            for (var x=0; x<16; x++) {
+                buffer[y * 16 * 3 + x * 3 + 0] = this.matrix[x][y].r * this.brightness;
+                buffer[y * 16 * 3 + x * 3 + 1] = this.matrix[x][y].g * this.brightness;
+                buffer[y * 16 * 3 + x * 3 + 2] = this.matrix[x][y].b * this.brightness;
+            }
+        }
+        
         this.spi.write(Buffer.concat([new Buffer([0x72]), buffer]), function (err) {
             if (err) {
                 throw 'Something went wrong!';
             }
         });
+        
     } else {
+        
         // log the matrix to the console
         for (var y=0; y<16; y++) {
             xStr = '';
@@ -109,6 +103,7 @@ UnicornHatHD.prototype.show = function (flip_horizontal, flip_vertical) {
             }
             console.log(xStr);
         }
+        
     }
 };
 
